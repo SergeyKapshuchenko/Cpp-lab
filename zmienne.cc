c54b04a8f97e101831f5e8747ab4bc62497fb306
SergeyKapshuchenko <serg3y1999@gmail.com>
2020-03-02 10:56:15 +0100

    2 zajecie koniec

--- zmienne.cc
+++ zmienne.cc
@@ -1,9 +1,40 @@
 #include<iostream>
+#include <iomanip>
 using namespace std;
 typedef int my_int;
 
 int main() {
     my_int zmienna = 100;
     cout << zmienna << endl;
+    int a = 1;
+    float b = 0.1;
+    double c = 0.01;
+    auto f = 0.03123;
+    char string[] = "test string";
+
+    int test1 = 3;
+    int test2 = 9;
+
+    cout << "Size of int: " << sizeof(a) << endl;
+    cout << "Size of float: " << sizeof(b) << endl;
+    cout << "Size of double: " << sizeof(c) << endl;
+    cout << "Size of auto: " << sizeof(f) << endl;
+    cout << "Size of char: " << sizeof(string) << endl;
+
+    cout << (double) test1/test2 << endl;
+    cout << test1 / double(test2) << endl;
+    cout << double(test1) / test2 << endl;
+
+    double c1 = static_cast<double>(test1) / test2;
+    double c2 = static_cast<double>(test1) /  static_cast<double>(test2);
+
+    cout << c1 << endl;
+    cout << c2 << endl;
+
+    std::cout << std::fixed << std::setprecision(2) << "c1: " << c1 << endl;
+    std::cout << std::fixed << std::setprecision(2) << "c2: " << c2 << endl;
+    std::cout << std::fixed << std::setprecision(2) << "a/b: " << test1/test2 << endl;
+
+
     return 0;
 }
